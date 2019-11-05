674f290cb3bac7576c37fb575759e24ff305af94
Razymex <amor.easa@gmail.com>
2019-11-05 08:36:44 -0500

    Adding google api

--- src/Core/Google/google.cs
+++ src/Core/Google/google.cs
@@ -12,6 +12,31 @@ namespace SheetsStart
 {
     class Program
     {
-        
+        static string[] Scopes = {SheetsService.Scope.Spreadsheets};
+        static string ApplicationName = "Discord Gear Bot";
+
+        static void Main(string[] args)
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("credentials.json",FileMode.Open,FileAccess.Read))
+                {
+                    string credPath = "token.json";
+                    credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                        GoogleClientSecrets.Load(stream).Secrets,
+                        Scopes,
+                        "user",
+                        CancellationToken.None,
+                        new FileDataStore(credPath, true)).Result;
+                    Console.WriteLine("Credential file saved to " + credPath);
+                    
+                    var service = new SheetsService(new BaseClientService.Initializer()
+                    {
+                        HttpClientInitializer = credential,
+                        ApplicationName = ApplicationName,
+                    });
+                }
+        }
     }
 }
